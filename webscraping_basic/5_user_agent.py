c44cfb98d7713a3a91c9d8841ff5ea97627a2418
eric.park <pjsin865@gmail.com>
2021-02-07 01:04:28 +0900

    test
    Signed-off-by: eric.park <pjsin865@gmail.com>

--- webscraping_basic/5_user_agent.py
+++ webscraping_basic/5_user_agent.py
@@ -5,5 +5,5 @@ headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 res = requests.get(url, headers=headers)
 res.raise_for_status()
 
-with open("nadocoding.html", "w", encoding="utf8") as f:
+with open("./webscraping_basic/nadocoding.html", "w", encoding="utf8") as f:
     f.write(res.text)
